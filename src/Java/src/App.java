0f045b4e7e4e4b942f6bb71c72ed51a9a09b864d
Angelo Santos <angelo.11@hotmail.com>
2022-03-04 00:43:58 -0300

    teste

---
+++ src/Java/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        int num = 5;
+        num = 5-2 ;
+        System.out.println("Hello, World! " + num);
+    }
+}
